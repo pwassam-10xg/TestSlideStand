4f16196278aca49d36ac7cd6380df0462dd7b8d9
Pascal Wassam <pascal.wassam@10xgenomics.com>
2022-11-14 19:53:34 -0800

    Largely reworked entry point

--- src/TestSlideStand/__main__.py
+++ src/TestSlideStand/__main__.py
@@ -1,29 +1,56 @@
 import logging
-from pathlib import Path
-import coloredlogs
-
-from TestSlideStand import TestSlideStand
+import time
+import tkinter as tk
+from tkinter import simpledialog
+import TestSlideStand
+from TestSlideStand.analysis import ImageAnalyzer
+from TestSlideStand.settings import settings
 
 def main():
-    coloredlogs.install(level=logging.DEBUG)
-    t = TestSlideStand(
-        TestSlideStand.Settings(
-            zaber_sn='AC01ZPBDA',
-        )
-    )
-    data = t.scan()
+    logging.basicConfig(format='%(asctime)s %(name)20s %(levelname)s %(message)s', level=logging.INFO)
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.INFO)
 
-    import png
-    basepath = Path('data')
-    basepath.mkdir(parents=True, exist_ok=True)
+    # Make sure data directory exists
+    settings.datadir.mkdir(parents=True, exist_ok=True)
 
-    for angle, img in data.items():
-        fname = basepath/f'{angle:.0f}.png'
-        logging.info("Writing %s", fname)
-        with open(fname, 'wb') as f:
-            w = png.Writer(width=img.shape[0], height=img.shape[1], greyscale=True)
-            w.write(f, img.T.copy())
-    return
+    while 1:
+        # Get slide ID
+        ROOT = tk.Tk()
+        ROOT.withdraw()
+        ref = simpledialog.askstring(title="User input", prompt="slide ID: ")
+
+        # Exit on empty slideid or dialog close
+        if not ref or not ref.strip():
+            break
+
+        fname = settings.datadir / (ref + f'_{int(time.time())}')
+
+        if fname.exists():
+            raise FileExistsError
+
+        fname.mkdir(exist_ok=False)
+
+        # Fork logging system into the directory
+        fh = logging.FileHandler(fname/'log.txt')
+        fh.setLevel(logging.INFO)
+        fh.setFormatter(logging.Formatter('%(asctime)s %(name)20s %(levelname)s %(message)s'))
+        logging.getLogger().addHandler(fh)
+        logging.info("Log start")
+        logging.info("TestSlideStand Version: %s", TestSlideStand.__version__)
+
+        t = TestSlideStand.TestSlideStand(settings, ref)
+        t.scan()
+        t.save(fname)
+        t.shutdown()
+        del t
+
+        a = ImageAnalyzer(ref)
+        a.load_offline(fname)
+        a.plot(fname)
+
+        logging.info("Log end")
+        logging.getLogger().removeHandler(fh)
+        fh.close()
 
 if __name__ == '__main__':
     main()
