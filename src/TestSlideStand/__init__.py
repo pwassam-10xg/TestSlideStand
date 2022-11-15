0611a6ffc72e4d50eff35468aeec3f89d3c7c930
Pascal Wassam <pascal.wassam@10xgenomics.com>
2022-11-14 19:54:11 -0800

    Combined changes

--- src/TestSlideStand/__init__.py
+++ src/TestSlideStand/__init__.py
@@ -1,16 +1,19 @@
 __version__ = '0.0.1'
 
 import logging
-from typing import Dict
-
-import coloredlogs
-
-from camera import Cam
-from stage import Stage
 import numpy as np
+import png
+
+from typing import Dict
+from dataclasses import dataclass
+from pathlib import Path
+
+from TestSlideStand.camera import Cam
+from TestSlideStand.stage import Stage
 from TestSlideStand.analysis import ImageAnalyzer
 
 __all__ = ['TestSlideStand']
+
 
 class TestSlideStand:
     @dataclass
@@ -18,15 +21,21 @@ class TestSlideStand:
         zaber_sn: str                                    # FTDI serial number of zaber
         positions: np.ndarray = np.arange(-30, 30, 2)    # Positions to scan
         auto_exp_pos: float = -30                        # Auto exposure position in degrees
+        datadir: Path = Path(r'c:\TestSlideStandData')
 
-    def __init__(self, settings: Settings):
+    def __init__(self, settings: Settings, ref: str):
         self.log = logging.getLogger(self.__class__.__name__)
         self.settings = settings
         self.log.info("Init")
 
         self.zaber = Stage(settings.zaber_sn)
         self.cam = Cam()
-        self.analyzer = ImageAnalyzer()
+        self.data: Dict[float, np.ndarray] = {}
+
+    def shutdown(self):
+        self.zaber.shutdown()
+        self.cam.shutdown()
+        return
 
     def scan(self) -> Dict[float, np.ndarray]:
         self.log.info("Scanning")
@@ -36,15 +45,13 @@ class TestSlideStand:
         exp = self.cam.auto_exposure()
         self.log.info("Auto exposure at %.1f uS", exp)
 
-        data = {}
         for p in self.settings.positions:
             self.log.info("Scanning position %.1f", p)
             self.zaber.abs(p)
             img = self.cam.snap()
-            data[p] = img
+            self.data[p] = img
 
         self.log.info("Scan Complete")
-        return data
 
     def save(self, datadir: Path):
         for angle, img in self.data.items():
